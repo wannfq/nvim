6419221d36c53e166dce843ee8c09655d4dbb507
Wan Afiq <wannfq@gmail.com>
2025-07-04 23:02:56 +0800

    Key bind for markdown preview

--- lua/wannfq/lazy/peek.lua
+++ lua/wannfq/lazy/peek.lua
@@ -2,14 +2,29 @@ return {
     {
         "toppair/peek.nvim",
         build = "deno task --quiet build:fast",
-        enabled = false,
-        event = { "VeryLazy" },
+        lazy = true,
+        event = "VeryLazy",
         config = function()
-            require("peek").setup({
+            local peek = require("peek")
+            peek.setup({
                 filetype = { 'markdown', 'conf' }
             })
-            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+            vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
+            vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+
+            vim.keymap.set(
+                { "n", "v" },
+                "<leader>md",
+                function()
+                    if peek.is_open() then
+                        peek.close()
+                    end
+                    peek.open()
+                end,
+                { 
+                    desc = "Markdown preview toggle"
+                }
+            )
         end,
     },
 }
